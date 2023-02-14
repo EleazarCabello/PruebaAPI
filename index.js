2f3d4efbd26cd1e3842c3d3a54078f4563277b20
EleazarCabello <L19100114@nlaredo.tecnm.mx>
2023-02-14 17:51:58 -0600

    .

---
+++ index.js
@@ -0,0 +1,16 @@
+const Frases=( 
+    "Más fe y menos miedo.",
+    "Todo lo puedo en Cristo que me fortalece.",
+    "Dios tiene un plan que lleva tu nombre.",
+    "Yo soy el camino, la verdad y la vida.",
+    "Solo debes dar un paso de fe y Dios se encargará de lo demás."
+    
+)
+
+function obtenerFrase(){
+    let indice = Math.floor(Math.random() * 5)
+    return Frases(indice)
+}
+
+module.exports.obtenerFrase=obtenerFrase;
+//console.log(module)
